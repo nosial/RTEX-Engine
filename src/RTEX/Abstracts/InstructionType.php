47b434b6258e746c3eba7a153bcd27bfe4a07e9a
Netkas <zixing.narrakas1996@gmail.com>
2022-12-27 02:47:49 -0500

    Added more instruction types to \RTEX\Abstracts > InstructionType

--- src/RTEX/Abstracts/InstructionType.php
+++ src/RTEX/Abstracts/InstructionType.php
@@ -4,7 +4,50 @@
 
     abstract class InstructionType
     {
+        // Base
         const Invoke = 'invoke';
+
+        // Variables
         const GetVariable = 'get';
         const SetVariable = 'set';
+
+        // Math
+        const Equals = 'equals';
+        const Sum = 'sum';
+        const Subtract = 'subtract';
+        const Divide = 'divide';
+        const Multiply = 'multiply';
+        const Modulo = 'modulo';
+        const Power = 'power';
+        const GreaterThan = 'greater_than';
+        const GreaterThanOrEquals = 'greater_than_or_equals';
+        const LessThan = 'less_than';
+        const LessThanOrEquals = 'less_than_or_equals';
+        const NotEquals = 'not_equals';
+
+        // Arrays
+        const ArrayGet = 'array_get';
+
+
+        const All = [
+            self::Invoke,
+
+            self::GetVariable,
+            self::SetVariable,
+
+            self::Equals,
+            self::Sum,
+            self::Subtract,
+            self::Divide,
+            self::Multiply,
+            self::Modulo,
+            self::Power,
+            self::GreaterThan,
+            self::GreaterThanOrEquals,
+            self::LessThan,
+            self::LessThanOrEquals,
+            self::NotEquals,
+
+            self::ArrayGet,
+        ];
     }
