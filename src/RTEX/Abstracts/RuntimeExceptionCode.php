fcfa860116280e7cbe0411899b468395c5b2998c
Netkas <zixing.narrakas1996@gmail.com>
2022-12-27 02:46:25 -0500

    Added \RTEX\Abstracts > RuntimeExceptionCode

---
+++ src/RTEX/Abstracts/RuntimeExceptionCode.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace RTEX\Abstracts;
+
+    abstract class RuntimeExceptionCode
+    {
+        const Exception = 0;
+        const AttributeException = -100;
+        const ImportException = -101;
+        const KeyException = -102;
+        const NameException = -103;
+        const TypeException = -104;
+        const ValueException = -105;
+        const ZeroDivisionException = -106;
+
+    }
